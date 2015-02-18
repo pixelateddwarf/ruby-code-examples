1309e40b0ceb99f5555eec3a37e288e5f6de7fd1
Alan Dunbar <pixelateddwarf@gmail.com>
2015-02-18 10:30:23 -0500

    updated fib2

--- fib2.rb
+++ fib2.rb
@@ -7,7 +7,6 @@
 #  Program: Fibonacci Sequence          #
 #  Purpose: Put in a number and get     #
 #           The fibonacci number for it #
-#           Array from 0 to 20          #
 #                                       #
 #  Programmer: Al Dunbar                #
 #  Date      : Feb 2015                 #
@@ -15,14 +14,19 @@
 #                                       #
 #########################################
 
-# Array of Fibonacci values for 0 to 20
-fib-number = [0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181,6765] 
+def fib(n)
+  ary = []
 
-puts "Enter a number 0 to 20:"
-
-my_num = gets.chomp
-my_num = my_num.to_i
-
-my_fib = fib-number(my_num)
-
-puts "f(#{my_num}) = #{my_fib} "
+  1.upto(n) do |n|
+  if n == 0 || n == 1
+      n
+  else
+    ary << fib(n-1) + fib(n-2)  ## put stuff into array
+  end
+end
+ 
+ary.last
+end
+ 
+ 
+puts fib(25) 
