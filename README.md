39e070f9ef64b56b89d5e2d127abc7007dc05138
pixelateddwarf <pixelateddwarf@gmail.com>
2015-02-19 20:36:03 -0500

    Modified fib2 and readme

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 These are just some of the small scripts that I have played around with.
 
-* FizBuz
-* Fibonacci - non array
-* Fib 2 - Array
+* FizBuz    - Fun game
+* Fibonacci - non array solution
+* Fib 2     - Array
--- fib2.rb
+++ fib2.rb
@@ -1,12 +1,12 @@
-#!/usr/bin/ruby
-
+ #!/usr/bin/env ruby -w
 
 #########################################
 #                                       #
 #                                       #
-#  Program: Fibonacci Sequence          #
-#  Purpose: Put in a number and get     #
-#           The fibonacci number for it #
+#  Program: Fib 2                       #
+#  Purpose: More efficient code, but    #
+#           longer solution - puts      #
+#           numbers into the array      #
 #                                       #
 #  Programmer: Al Dunbar                #
 #  Date      : Feb 2015                 #
@@ -14,19 +14,37 @@
 #                                       #
 #########################################
 
-def fib(n)
-  ary = []
-
-  1.upto(n) do |n|
-  if n == 0 || n == 1
-      n
-  else
-    ary << fib(n-1) + fib(n-2)  ## put stuff into array
+require 'minitest/autorun'
+module Fib
+  def self.fib(n)
+      arry = [1,1]
+      if n == 0 
+        return []
+      elsif n == 1
+        return [1]        
+      end
+      items_to_add = n-2
+      items_to_add.times do
+        fib_prev =  arry[arry.length-1]
+        fib_2x   =  arry[arry.length-2]
+        the_stuff = fib_prev + fib_2x
+        arry << the_stuff
+      end
+      return arry
   end
 end
- 
-ary.last
+
+class TestFib < Minitest::Test
+  def test_fib
+    actual = Fib.fib(5)                    ## put the number you want to be displayed here
+    expected =[1,1,2,3,5]
+    assert_equal expected, actual
+  end
+
+  def test_fib_0
+    actual = Fib.fib(0)
+    expected =[]
+    assert_equal expected, actual
+  end
+
 end
- 
- 
-puts fib(25) 
